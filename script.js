bf5585de3aac2cef63d71393d69c3e4f621d5f61
shreyakalla02 <150719217+shreyakalla02@users.noreply.github.com>
2025-03-20 18:33:32 +0000

    End of session

--- script.js
+++ script.js
@@ -13,3 +13,23 @@ document.getElementById("id").value;
 document.getElementById("id").addEventListener("click", function);
 document.addEventListener("DOMContentLoaded", function() {  });
 */
+
+function jcp() {
+    console.log("Button Clicked");
+    gender = document.getElementById("genderdd").value;
+    breed = document.getElementById("breedd").value;
+    ageinYears = document.getElementById("agedd").value;
+
+    avgTreatmentCost = petdata[ageinYears]["costOfTreatment"];
+    illnessChance = parseFloat(petdata[ageinYears]["ill"])/100;
+    ev = (avgTreatmentCost * illnessChance ) * (1+gender) * (1+breed);
+    ev = ev.toFixed(2);
+
+    console.log(`Premium is: ${ev}`);
+    document.getElementById("premcalc").innerHTML = "$"+ev;
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    // jcp();
+    document.getElementById("calcbtn").addEventListener("click", jcp);
+});
