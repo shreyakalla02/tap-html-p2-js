67ef9ecb1b128dce4a9115bd103f551104fdc16f
ckechios <62009820+ckechios@users.noreply.github.com>
2025-03-20 13:34:28 +0000

    added data for calc

---
+++ script.js
@@ -0,0 +1,8 @@
+// Data for calculation
+petdata = {
+    "1" : { ill : "6%", costOfTreatment: 100 },
+    "2" : { ill : "5%", costOfTreatment: 200 },
+    "3" : { ill : "4%", costOfTreatment: 300 },
+    "4" : { ill : "3%", costOfTreatment: 400 },
+    "5" : { ill : "2%", costOfTreatment: 500 },
+}
